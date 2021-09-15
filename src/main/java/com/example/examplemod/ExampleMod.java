7df7e442101f55a038d44ef2ce4cd8174c753bd5
朱峰 <386408003@qq.com>
2021-09-15 10:48:08 +0800

    IDEA不支持自带Gradle版本，所以升级为3.0，第一个Mod成功运行。

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,8 +10,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "firstmod";
+    public static final String NAME = "First Mod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -27,5 +27,6 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("ZhuFeng's First Mod Loaded!");
     }
 }
